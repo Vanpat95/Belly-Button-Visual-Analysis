ceb6e1360b8bd2fe3e186eb4a770b0055949cb8d
Vandan Patel <vp51095@gmail.com>
2022-12-12 21:33:58 -0800

    updating homework

---
+++ Starter_Code/StarterCode/static/js/app.js
@@ -0,0 +1,96 @@
+function metaData(sample) {
+    d3.json("./samples.json").then((data) => {
+    let metadata = data.metadata;
+    let dataArray = metadata.filter(filterData => filterData.id == sample);
+    let results = dataArray[0];
+    let dropdownMenu = d3.select("#sample-metadata");
+    dropdownMenu.html("")
+    for (metaDatas in results){
+        dropdownMenu.append("h6").text(`${metaDatas}: ${results[metaDatas]}`)
+    }  
+})
+    
+}
+
+function charts(sample) {
+    d3.json("./samples.json").then((data) => {
+    console.log("test")
+    let metadata = data.samples;
+    let dataArray = metadata.filter(filterData => filterData.id == sample);
+    let results = dataArray[0];
+    let otu_ids = results.otu_ids;
+    let sample_values = results.sample_values;
+    let otu_labels = results.otu_labels;
+
+    let trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        
+        type: "bubble"
+      };
+    
+    // Data trace array
+    let traceData = [trace1];
+    
+    // Apply the group barmode to the layout
+    let layout = {
+      title: "Bellybutton Bar Chart"
+    };
+    
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("bubble", traceData, layout);
+    
+
+
+    })
+}
+
+// let trace1 = {
+//   x: otu_ids,
+//   y: sample_values,
+//   type: "bubble"
+// };
+
+// // Data trace array
+// let traceData = [trace1];
+
+// // Apply the group barmode to the layout
+// let layout = {
+// title: "Bellybutton Bar Chart"
+// };
+
+// // Render the plot to the div tag with id "plot"
+// Plotly.newPlot("bubble", traceData, layout);
+
+
+
+// })
+// }
+
+
+function init(){
+  d3.json("./samples.json").then(function(data){
+    console.log(data)
+  
+  // let data = d3.json("../../samples.json");
+  let selection = d3.select("#selDataset");
+  let id = data.names;
+
+  for (let i = 0; i < id.length; i++){
+    selection 
+      .append("option")
+      .text(id[i])
+      .property("value",id[i]);
+  };
+
+  let data2 = id[0];
+  charts(data2);
+  metaData(data2);
+})
+}
+init();
+
+function optionChanged(new_id){
+  charts(new_id);
+  metaData(new_id);
+}
